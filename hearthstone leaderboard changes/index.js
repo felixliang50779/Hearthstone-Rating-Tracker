60008bbaff8e198bb1a85547fa06c20dd7f4f063
EricW21 <ew21hi@gmail.com>
2023-08-16 16:37:18 -0400

    Implemented fetching user data from API

---
+++ hearthstone leaderboard changes/index.js
@@ -0,0 +1,50 @@
+
+const PORT = 5000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const nodemon = require("nodemon");
+const app = express();
+let html_data;
+TrackedPlayers = [ 'dog', 'Lii', 'SBR', 'jeef', 'BeterBabbit', 'Bofur', 'MATSURI', 'awedragon', 'jkirek'];
+TrackPlayerTable = {};
+let pagenum=1;
+let pageData;
+let url;
+fetchLoop().then(() => console.log(TrackPlayerTable));
+
+
+app.get("/",(req,res)=>res.send(TrackPlayerTable));
+
+
+app.listen(PORT, () =>
+  console.log(`The server is active and running on port ${PORT}`)
+);
+
+async function fetchLoop(){
+    const requests = [];
+    for (let i = 1; i < 11; i++) {
+        pagenum = i;
+        url = `https://hearthstone.blizzard.com/en-us/api/community/leaderboardsData?region=US&leaderboardId=battlegrounds&page=${pagenum}`;
+        
+        requests.push(axios.get(url));
+    }
+    const responses = await Promise.all(requests);
+    for (const response of responses) {
+        html_data = response.data;
+        pageData = html_data.leaderboard.rows;
+        let playerRating;
+        for (const playerData of pageData) {
+            if (TrackedPlayers.includes(playerData.accountid)) {
+                playerRating = playerData.rating;
+
+                if (!TrackPlayerTable[playerData.accountid]) {
+                    TrackPlayerTable[playerData.accountid] = [];
+                }
+
+                TrackPlayerTable[playerData.accountid].push([playerRating,"8/16/2023 9:00 pm"]);
+            }
+        }
+    }
+}
+
