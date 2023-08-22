5221d019c9d1b9ae5d3919564215beb52a99ee65
Felix Liang <liangfel@msu.edu>
2023-08-22 19:12:07 -0400

    Added helper components used in cloud function

---
+++ hearthstone leaderboard changes/databaseController.js
@@ -0,0 +1,48 @@
+import { MongoClient } from 'mongodb';
+
+class DatabaseController {
+    constructor(url, database) {
+        this.client = new MongoClient(url);
+        this.database = this.client.db(database);
+    }
+
+    async shutdownClient() {
+        try {
+            this.client.close();
+        }
+        catch (error) {
+            console.log(`${errorMsg} disconnect: ${error}`);
+        }
+    }
+
+    // Gets a single document from a specific collection
+    async getDocument(collection, documentID) {
+        const myCollection = this.database.collection(collection);
+        
+        return myCollection.findOne({_id: documentID});
+    }
+
+    async pushToDocument(value, documentID, arrayName, collection) {
+        await this.database.collection(collection).updateOne(
+            { "_id": documentID },
+            { "$push":
+                {
+                    [`players.${arrayName}`]: value
+                }
+            }
+        );
+    }
+
+    async deleteFromDocument(value, documentID, arrayName, collection) {
+        await this.database.collection(collection).updateOne(
+            { "_id": documentID },
+            { "$pull":
+                {
+                    [`players.${arrayName}`]: value
+                }
+            }
+        )
+    }
+}
+
+export { DatabaseController };
---
+++ hearthstone leaderboard changes/fetchLoop.js
@@ -0,0 +1,96 @@
+import axios from "axios";
+import { DatabaseController } from "./databaseController.js";
+
+const fetchLoop = async () => {
+
+    // Initialize controller for a database at the cluster url we indicate
+    const dbController = new DatabaseController(
+        "mongodb+srv://fliang2024:SpveX9eUid8w60pd@cluster0.ispagpy.mongodb.net/?retryWrites=true&w=majority", 
+        "hearthstone-battlegrounds-records");
+
+    // Get most up-to-date player records
+    const TrackedPlayersDocument = await dbController.getDocument(
+        "player-records", "TrackedPlayers");
+    const TrackedPlayers = TrackedPlayersDocument.players;
+
+    // Initialize counter to track how many players' records
+    // we've updated
+    const numPlayers = Object.keys(TrackedPlayers).length;
+    let playersUpdated = 0;
+
+    const date = new Date();
+    const pageRequests = [];
+
+    for (let i = 1; i < 9; i++) {
+        const url = `https://hearthstone.blizzard.com/en-us/api/community/leaderboardsData?region=US&leaderboardId=battlegrounds&page=${i}`;
+        
+        pageRequests.push(axios.get(url));
+    }
+
+    // Wait for page requests to resolve concurrently
+    const pages = await Promise.all(pageRequests);
+
+    const updatePromises = [];
+
+    for (const page of pages) {
+        const pageData = page.data.leaderboard.rows;
+
+        for (const playerData of pageData) {
+            const playerRecords = TrackedPlayers[playerData.accountid];
+            if (playerRecords) {
+                if ((!playerRecords.length) || 
+                    (playerRecords.at(-1).rating !== playerData.rating)) {
+                        updatePromises.push(dbController.pushToDocument({rank: playerData.rank, 
+                            rating: playerData.rating, timeStamp: date}, 
+                            "TrackedPlayers", playerData.accountid, "player-records"));
+                }
+                playersUpdated += 1;
+
+                // We've checked all the players we're tracking
+                if (playersUpdated === numPlayers) {
+                    break;
+                }
+            }
+        }
+        // Don't need to keep searching
+        // Break out of the updating loop to optimize performance
+        if (playersUpdated === numPlayers) {
+            break;
+        }
+    }
+
+    // If the records were updated, we resolve all the requests
+    // and remove any records from > 2 wks ago
+    if (updatePromises.length) {
+        await Promise.all(updatePromises);
+    }
+
+    const clearedRecords = [];
+
+    for (const playerRecord in TrackedPlayers) {
+        for (const record of TrackedPlayers[playerRecord]) {
+            if (date.getTime() - record.timeStamp.getTime() > 1209600000) {
+                clearedRecords.push(
+                    dbController.deleteFromDocument(
+                        record, "TrackedPlayers", playerRecord, "player-records"));
+            }
+            else {
+                break;
+            }
+        }
+    }
+    
+    if (clearedRecords.length) {
+        await Promise.all(clearedRecords);
+    }
+
+    dbController.shutdownClient();
+
+    return TrackedPlayers;
+
+    // res.status(200).send('Success!');
+}
+
+fetchLoop().then(TrackedPlayers => console.log(TrackedPlayers));
+
+export { fetchLoop };
