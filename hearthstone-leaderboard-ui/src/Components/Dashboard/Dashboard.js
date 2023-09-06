68a4c6b7c07372ea7090ed554df01712c62249c4
Felix Liang <liangfel@msu.edu>
2023-09-06 14:29:14 -0400

    Added dashboard sidebar

--- hearthstone-leaderboard-ui/src/Components/Dashboard/Dashboard.js
+++ hearthstone-leaderboard-ui/src/Components/Dashboard/Dashboard.js
@@ -1,15 +1,13 @@
-import { Fragment, useState } from "react";
+// import { useState } from "react";
 
-import { LineGraph } from "../Graph/LineGraph";
-import { DataTable } from "../Table/DataTable";
+// import { LineGraph } from "../Graph/LineGraph";
+// import { DataTable } from "../Table/DataTable";
+import { DefaultSidebar } from "./Sidebar/Sidebar";
 
 export function Dashboard({ fetchResult, timeDisplay }) {
-    const selectPlayer = useState(Object.keys(fetchResult).at(0));
+    // const selectPlayer = useState(Object.keys(fetchResult).at(0));
 
     return (
-        <Fragment>
-            <LineGraph fetchResult={fetchResult} selectPlayer={selectPlayer} />
-            <DataTable fetchResult={fetchResult} selectPlayer={selectPlayer} timeDisplay={timeDisplay} />
-        </Fragment>
+        <DefaultSidebar />
     );
 }
---
+++ hearthstone-leaderboard-ui/src/Components/Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,96 @@
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemPrefix
+} from "@material-tailwind/react";
+
+import GuestIcon from '../../../Assets/Images/Guest.png';
+import BattlegroundsIcon from '../../../Assets/Images/Battlegrounds.webp';
+import StandardIcon from '../../../Assets/Images/Standard.webp';
+import ClassicIcon from '../../../Assets/Images/Classic.webp';
+import MercenariesIcon from '../../../Assets/Images/Mercenaries.webp';
+import ArenaIcon from '../../../Assets/Images/Arena.webp';
+import TwistIcon from '../../../Assets/Images/Twist.webp';
+import WildIcon from '../../../Assets/Images/Wild.webp';
+
+import { Avatar, Text } from 'dracula-ui';
+ 
+export function DefaultSidebar() {
+  return (
+    <Card className="h-[calc(100vh)] w-full max-w-[18rem] p-6" style={{ backgroundColor: "#282a36", alignSelf: "center"}} >
+      <div>
+        <Avatar color="white" title="Guest" src={GuestIcon} style={{ marginLeft: "5rem" }}/>
+        <div style={{ marginLeft: "5.7rem" }}>
+          <Text color="white">Guest</Text>
+        </div>
+      </div>
+      <hr style={{ color: "#f8f8f2", opacity: 0.5, marginTop: "1rem" }} className="py-1" />
+      <List>
+        <ListItem style={{ color: "white" }} className="-m-2 p-6">
+          <ListItemPrefix className="h-2">
+            <img
+              src={BattlegroundsIcon}
+              alt="BattlegroundsIcon"
+              width={40} />
+          </ListItemPrefix>
+          <Text className="px-11">Battlegrounds</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="p-5">
+          <ListItemPrefix className="h-2">
+            <img
+              src={StandardIcon}
+              alt="StandardIcon"
+              width={30} />
+          </ListItemPrefix>
+          <Text className="px-10">Standard</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="-m-1 p-6">
+          <ListItemPrefix className="h-2">
+            <img
+              src={ClassicIcon}
+              alt="ClassicIcon"
+              width={30} />
+          </ListItemPrefix>
+          <Text className="px-10">Classic</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="-m-1 p-5">
+          <ListItemPrefix className="h-2">
+            <img
+              src={MercenariesIcon}
+              alt="MercenariesIcon"
+              width={40} />
+          </ListItemPrefix>
+          <Text className="px-11">Mercenaries</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="-m-2 p-6">
+          <ListItemPrefix className="h-2">
+            <img
+              src={ArenaIcon}
+              alt="ArenaIcon"
+              width={40} />
+          </ListItemPrefix>
+          <Text className="px-11">Arena</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="-m-2 p-6">
+          <ListItemPrefix className="h-2">
+            <img
+              src={TwistIcon}
+              alt="TwistIcon"
+              width={40} />
+          </ListItemPrefix>
+          <Text className="px-11">Twist</Text>
+        </ListItem>
+        <ListItem style={{ color: "white" }} className="-m-2 p-6">
+          <ListItemPrefix className="h-2">
+            <img
+              src={WildIcon}
+              alt="WildIcon"
+              width={40} />
+          </ListItemPrefix>
+          <Text className="px-11">Wild</Text>
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
